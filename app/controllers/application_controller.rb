0ec55b4b9b38261e63fb10350f1643eaab1c384c
aka147 <ajajtajpgj@gmail.com>
2020-10-22 10:47:14 +0000

    7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+  protect_from_forgery with: :exception
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+
 
   def after_sign_in_path_for(resource)
     user_path(current_user) # ログイン後に遷移するpathを設定
@@ -13,6 +11,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     root_path # ログアウト後に遷移するpathを設定
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :books
-  resources :users
+  resources :users, only: [:index,:show]
   root 'home#top'
   get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
